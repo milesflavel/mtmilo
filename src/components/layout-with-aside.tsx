fc16041046be227037f988c4d60cc4abd8d61438
unknown <milesflavel@gmail.com>
2023-12-17 18:20:59 +0800

    Fix layout responsiveness and center content

--- src/components/layout-with-aside.tsx
+++ src/components/layout-with-aside.tsx
@@ -5,10 +5,10 @@ const LayoutWithAside = (props: {
   aside?: React.ReactNode;
 }) => {
   return (
-    <div className="flex h-full flex-col flex-nowrap gap-4 overflow-auto md:flex-row md:justify-between">
-      <div className="h-fit w-full md:mx-auto md:max-w-2xl">
+    <div className="flex h-full flex-col flex-nowrap gap-4 overflow-auto md:flex-row md:justify-center">
+      <div className="h-fit w-full md:h-full md:max-w-2xl">
         <Pane className="flex flex-grow justify-center p-4 md:h-full">
-          <main className="w-full px-4">{props.main}</main>
+          <main className="w-full overflow-auto px-4">{props.main}</main>
         </Pane>
       </div>
       <Pane className="flex h-fit w-full justify-center p-4 md:w-80">
