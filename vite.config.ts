c361b50bc3ad1bb5f579d6e4159f4f41fd506b1b
unknown <milesflavel@gmail.com>
2023-12-31 16:42:49 +0800

    Add auto-generating sitemap XML

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,24 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import svgr from "vite-plugin-svgr";
+import Sitemap from "vite-plugin-sitemap";
 import { visualizer } from "rollup-plugin-visualizer";
+import BlogIndex from "./public/blog/index.json";
+
+const dynamicRoutes = [
+  "/",
+  ...BlogIndex.map((article) => `/blog/${article.id}`),
+];
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     svgr(),
+    Sitemap({
+      dynamicRoutes: dynamicRoutes,
+      hostname: "https://www.mtmilo.net/",
+    }),
     visualizer(), // Must be last entry
   ],
   assetsInclude: ["**/*.svg"],
