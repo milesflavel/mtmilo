7a4e24f6b854aaa5570b6f588f906a5aaa9c6f87
unknown <milesflavel@gmail.com>
2024-01-03 18:56:10 +0800

    Remove background svg from accessibility tree

--- src/components/app-background.tsx
+++ src/components/app-background.tsx
@@ -2,7 +2,7 @@ import AppBackgroundSvg from "../images/app-background.svg?react";
 
 const AppBackground = () => (
   <div className="absolute left-0 top-0 -z-10 h-full w-full">
-    <AppBackgroundSvg />
+    <AppBackgroundSvg aria-hidden />
   </div>
 );
 
